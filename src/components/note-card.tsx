816b26be5aa88986708e2040517cf9025b17cc8a
João Victor Peres Silva <vitorr-joao2222@hotmail.com>
2024-02-09 23:55:10 -0300

    aula-2

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,19 +1,55 @@
-export function NoteCard() {
-  return (
-    <button className='text-left rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-slate-600 hover:ring-2 focus-visible:ring-lime-400'>
-      <span className='text-sm font-medium text-slate-300'>há 2 dias</span>
-      <p className='text-sm leading-6 text-slate-400'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-        necessitatibus unde reiciendis perspiciatis. Dolorem culpa, omnis odit
-        officia eos error quasi iusto sapiente architecto vel officiis
-        distinctio dolore consequuntur minima. Lorem, ipsum dolor sit amet
-        consectetur adipisicing elit. Excepturi, tempore cupiditate nisi
-        consequatur eaque perspiciatis placeat iste, quisquam officia voluptate
-        accusantium vel illo perferendis. Ab accusantium voluptas distinctio
-        fuga repellendus?
-      </p>
+import * as Dialog from '@radix-ui/react-dialog';
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { X } from 'lucide-react';
 
-      <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
-    </button>
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({ note }: NoteCardProps) {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger className='text-left rounded-md flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
+        <span className='text-sm font-medium text-slate-300'>
+          {note.date.toISOString()}
+        </span>
+        <p className='text-sm leading-6 text-slate-400'>{note.content}</p>
+
+        <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className='inset-0 fixed bg-black/60' />
+        <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col'>
+          <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
+            <X className='size-5' />
+          </Dialog.Close>
+          <div className='flex flex-1 flex-col gap-3 p-5'>
+            <span className='text-sm font-medium text-slate-300'>
+              {formatDistanceToNow(note.date, {
+                locale: ptBR,
+                addSuffix: true,
+              })}
+            </span>
+
+            <p className='text-sm leading-6 text-slate-400'>{note.content}</p>
+          </div>
+
+          <button
+            type='button'
+            className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'
+          >
+            Deseja{' '}
+            <span className='text-red-400 group-hover:underline'>
+              apagar essa nota?
+            </span>
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './app';
+import { Toaster } from 'sonner';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Toaster richColors />
   </React.StrictMode>
 );
