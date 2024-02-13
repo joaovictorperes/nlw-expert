8c01494dcf4b3a213bb29a653d3ad82e217dd4ab
João Victor Peres Silva <vitorr-joao2222@hotmail.com>
2024-02-13 08:08:16 -0300

    scroll-corrections

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -22,14 +22,16 @@ export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
             addSuffix: true,
           })}
         </span>
-        <p className='text-sm leading-6 text-slate-400'>{note.content}</p>
+        <p className='text-sm leading-6 text-slate-400 break-all overflow-y-auto'>
+          {note.content}
+        </p>
 
         <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
       </Dialog.Trigger>
 
       <Dialog.Portal>
         <Dialog.Overlay className='inset-0 fixed bg-black/60' />
-        <Dialog.Content className='fixed inset-0 md:inset-auto overflow-hidden md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col'>
+        <Dialog.Content className='fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col'>
           <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
             <X className='size-5' />
           </Dialog.Close>
@@ -41,7 +43,9 @@ export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
               })}
             </span>
 
-            <p className='text-sm leading-6 text-slate-400'>{note.content}</p>
+            <p className='text-sm leading-6 text-slate-400 break-all overflow-y-scroll md:h-[40vh]'>
+              {note.content}
+            </p>
           </div>
 
           <button
